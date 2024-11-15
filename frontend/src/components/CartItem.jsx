3c8891f99deca4e3d698d9dfa11094a1331c09c1
harishchandragiri <giriharishchandra9999@gmail.com>
2024-11-15 10:50:06 +0545

    cartItems finished

--- frontend/src/components/CartItem.jsx
+++ frontend/src/components/CartItem.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import {Link} from "react-router-dom";
+import {FaTrash} from 'react-icons/fa';
 
-function CartItem() {
+function CartItem({cartItem}) {
+  const {photo, productId, name, price, stock} = cartItem;
+
   return (
-    <div>CartItem</div>
+    <div className='w-full h-[200px]  border m-1 my-1 flex items-center'>
+      <img className='w-[150px] h-[150px] border m-1' src={photo} alt={name} />
+   <div className='flex items-center ml-auto space-x-10 m-2'>
+   <article>
+       <Link className='font-bold' to={`/product/${productId}`}>{name}</Link>
+       <br />
+       <span className='font-bold'>Rs. {price}</span>
+      </article>
+      <div>
+        <button className=' border border-black w-[25px] rounded-md border-[2px] bg-yellow-200 m-1'>-</button>
+        <p className='font-bold m-1'>{stock}</p>
+        <button className=' border border-black w-[25px] rounded-md border-[2px] bg-yellow-200 m-1'>+</button>
+      </div>
+      <button>
+        <FaTrash></FaTrash>
+      </button>
+   </div>
+    </div>
   )
 }
 
-export default CartItem
+export default CartItem;
--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,7 +1,44 @@
 import React, { useState } from 'react'
 import { useEffect } from 'react';
+import CartItem from '../components/CartItem';
 
-const items= [
+const cartItems= [
+{
+  productId:'asdfgfdkj123',
+  name:'mackbook',
+  stock:40,
+  price:10000,
+  photo:'asmjckdjdsnsajkdbehdbsakbsaddjbdaj'
+},
+{
+  productId:'vy12j123',
+  name:'bike',
+  stock:2,
+  price:1045000,
+  photo:'12132jhsgcgdhfbsakbsaddjbdaj'
+},
+{
+  productId:'vy12j123',
+  name:'bike',
+  stock:2,
+  price:1045000,
+  photo:'12132jhsgcgdhfbsakbsaddjbdaj'
+},
+{
+  productId:'12gfdkj123',
+  name:'samsung',
+  stock:5,
+  price:15000,
+  photo:'dsnsajkdbehdbsakbsaddjbdaj'
+},
+{
+  productId:'vy12j123',
+  name:'bike',
+  stock:2,
+  price:1045000,
+  photo:'12132jhsgcgdhfbsakbsaddjbdaj'
+}
+
 
 ]
 const subtotal=10000;
@@ -27,20 +64,26 @@ function Cart() {
   }, [couponCode]);
 
   return (
-    <div className='block sm:flex'>
-      <main className='w-full sm:w-[75vw] mx-1 h-56'>
+    <div className='flex'>
+      <main className='w-[80vw] sm:w-[75vw] mx-1'>
         {/* main content of the main tag */}
+        {
+          cartItems.length > 0 ? (
+            cartItems.map((i, index)=> <CartItem key={index} cartItem={i} /> )
+          ) : (
+              <h1>No Items Added</h1>
+            )
+        }
 
-        
       </main>
-      <aside className='w-full sm:w-[25vw] mx-1'>
+      <aside className='sm:w-[25vw] mx-1'>
        <div className='text-left p-2 m-1'>
         <p className='my-1'>Subtotal: Rs. {subtotal}</p>
         <p className='my-1'>Shipping Charge: Rs. {shippingCharge}</p>
         <p className='my-1'>Tax: Rs. {tax}</p>
         <p className='my-1'>Discount: Rs. {discount}</p>
         <p className='my-1'><b>Total: Rs. {total}</b></p>
-        <input className='border-2 border-black rounded-md w-[150px] my-1 p-1' 
+        <input className='border-2 border-black rounded-md sm:w-[150px] w-[110px] my-1 p-1' 
         type="text" 
         placeholder='couponCode' 
         value={couponCode} 
