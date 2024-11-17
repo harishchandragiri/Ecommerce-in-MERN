766181104bfb5717f68a3daa09d2ce29946a4030
harishchandragiri <giriharishchandra9999@gmail.com>
2024-11-17 16:22:30 +0545

    login styled

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,17 +13,19 @@ function Login() {
                 <div className=' border rounded-md m-2 shadow-md h-[350px] w-[300px]'>
                 <h1 className='block mt-2 h-[40px] text-[20px] font-bold'>Login</h1>
                 <hr className='border-[1px] border-black' />
-                <form >
-                <label className=' h-[40px] my-[10px] w-[80px] font-bold'>Name</label>
-                <br />
+                <form className='relative'>
+                <div className='relative w-full h-[25px]'>
+                  <label className='absolute left-[50px] h-[40px] font-bold'>Name</label>
+                </div>
                 <input className=' h-[40px] rounded-md p-1 border-black border-[2px] my-2 w-[200px]'  type="text" value={Name} onChange={(e)=> SetName(e.target.value)}/>
-                <br />
-                <label className=' h-[40px] my-[10px] font-bold'>Date</label>
-                <br />
+                <div className='relative w-full h-[25px]'>
+                  <label className='absolute left-[50px] h-[40px] font-bold'>Date of Birth</label>
+                </div>
                 <input className=' h-[40px] rounded-md p-1 border-black border-[2px] my-2 w-[200px]'  type="date" value={date} onChange={(e)=> Setdate(e.target.value)}/>
-                <br />
-                <label className=' h-[40px] my-[10px] font-bold'>Gender</label>
-                <div className='h-[40px]'>
+                <div className='relative w-full h-[25px]'>
+                  <label className='absolute left-[50px] h-[40px] font-bold'>Gender</label>
+                </div>
+                <div className='h-[40px] w-full'>
                 <label className='m-2' htmlFor="male">Male</label>
                 <input type="radio" 
                        id='male'
