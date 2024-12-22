4903d07adcbf283fc1e1e3fda32503a5f7623cb0
harishchandragiri <giriharishchandra9999@gmail.com>
2024-12-22 07:55:15 +0545

    db connected

--- backend/constants.js
+++ backend/constants.js
@@ -1,1 +1,1 @@
-export const DB_NAME= ecomerceDB;
+export const DB_NAME= 'ecomerceDB';
---
+++ backend/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants";
+
+const DB_CONNECTION = async() =>{
+    try{
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        console.log(`\n MONGODB connected !! DB HOST : ${connectionInstance.connection.host}`)
+    }catch (error){
+        console.log("MONGODB connection error", error)
+        process.exit(1);
+    }
+}
+
+export default DB_CONNECTION;
