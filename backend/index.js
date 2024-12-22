6ac42665c5946f7a7bbb8a0805ca8048b492dec7
harishchandragiri <giriharishchandra9999@gmail.com>
2024-12-22 13:22:47 +0545

    database connected

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from 'dotenv';
+import DB_CONNECTION from './db/index.js';
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3003;
 
+// Database connection 
+DB_CONNECTION();
 
 
  app.listen(port, ()=>{
