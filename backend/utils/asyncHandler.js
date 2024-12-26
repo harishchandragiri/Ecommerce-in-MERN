b15ead8082a36ecb1fca43302d2db990b1582ea8
harishchandragiri <giriharishchandra9999@gmail.com>
2024-12-26 18:03:48 +0545

    models addel

--- backend/utils/asyncHandler.js
+++ backend/utils/asyncHandler.js
@@ -1,7 +1,7 @@
 const asyncHandler = (requestHandler) => {
     return(req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).
-        catch((error)=>next(error))
+        Promise.resolve(requestHandler(req, res, next))   //.then is not necessary to consume so .catch is is directly applied
+        .catch((error)=>next(error))
     }
 }
 
